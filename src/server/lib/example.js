0f61349228e13c023565ebb7f15d2082250e58af
Hélio Rocha <hjrocha@gmail.com>
2019-10-12 08:51:59 +0100

    some more methods

--- src/server/lib/example.js
+++ src/server/lib/example.js
@@ -62,3 +62,21 @@ exports.ransomNote = function(note, magazine) {
 
   return possible;
 };
+
+exports.pluralize_v2 = function (count, singular, plural) {
+  if (count === 1) {
+    return `${count} ${singular}`;
+  }
+  plural = plural || `${singular}s`;
+  return `${count} ${plural}`;
+};
+
+exports.method = function (count) {
+  for(let i = 0; i < count; i++){
+    const j = 9
+    while(j>count){
+      console.log("counting")
+      j--
+    }
+  }
+};
