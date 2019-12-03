5f96e62399c2a81d0604175c22a590e17f2bcbfc
Hélio Rocha <hjrocha@gmail.com>
2019-12-03 11:36:01 +0000

    some new methods

--- src/server/lib/example.js
+++ src/server/lib/example.js
@@ -15,3 +15,38 @@ exports.mul = function (a,b) {
 };
 
 
+exports.fibonacci = function(num){
+  var a = 1, b = 0, temp;
+
+  while (num >= 0){
+    temp = a;
+    a = a + b;
+    b = temp;
+    num--;
+  }
+
+  return b;
+}
+
+
+exports.ransomNote = function(note, magazine) {
+  const magazineWords = magazine.split(" ");
+  const magazineHash = {};
+
+  magazineWords.forEach(word => {
+      if (!magazineHash[word]) magazineHash[word] = 0;
+      magazineHash[word]++;
+  });
+
+  const noteWords = note.split(" ");
+  let possible = true;
+
+  noteWords.forEach(word => {
+      if (magazineHash[word]) {
+          magazineHash[word]--;
+          if (magazineHash[word] < 0) possible = false;
+      } else possible = false;
+  });
+
+  return possible;
+};
