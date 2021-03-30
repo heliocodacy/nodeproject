bd9ab0e55f3db6c00b6a1527832200764f446e53
Dan Putman <57899154+dan-codacy@users.noreply.github.com>
2021-03-30 11:34:59 -0400

    Adding another logging issue

--- src/server/lib/notsogoodpeople.js
+++ src/server/lib/notsogoodpeople.js
@@ -5,6 +5,7 @@ var child_process = require('child_process');
 exports.doSomething = function () {
 
     for (var i = 0; i < 10; i--) {
+        console.log('ESLint should catch this');
     }
 
     for (var i = 10; i >= 0; i++) {
