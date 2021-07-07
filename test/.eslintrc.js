a8ab559a705edf9744ec65ee8bc87863e5f5e74e
heliocodacy <46491761+heliocodacy@users.noreply.github.com>
2021-07-07 11:56:00 +0100

    Update .eslintrc.js

--- test/.eslintrc.js
+++ test/.eslintrc.js
@@ -1,5 +1,11 @@
 module.exports = {
     "rules": {
         "quotes": [2, "double"]
+    },
+    "parserOptions": {
+    "ecmaFeatures": {
+        "jsx": true,
+        "modules": true
     }
+}
 }
